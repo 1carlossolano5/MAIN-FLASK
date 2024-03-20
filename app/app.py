1f58e02ad815a77172c26bf4f88027b58561ba57
APRENDIZ <SENA@BOGDAFCGFFSD676.FormacionSena.RED>
2024-03-20 09:48:07 -0500

    nashe

--- app/app.py
+++ app/app.py
@@ -1,6 +1,7 @@
 from flask import Flask,render_template,redirect,url_for,flash, request,session
 import mysql.connector
 from werkzeug.security import generate_password_hash, check_password_hash
+import base64 
 #Creamos una instancia de la clase Flask
 
 app = Flask(__name__)
@@ -74,7 +75,7 @@ def logout():
 
 #Definir rutas
 
-@app.route('/')
+@app.route('/lista')
 def lista():
     cursor = db.cursor()
     cursor.execute('SELECT * FROM personas')
@@ -107,7 +108,7 @@ def registrar_usuario():
             print('El usuario o correo ya existe')
             return render_template('Registrar.html')
 
-        cursor.execute("INSERT INTO personas(nombreper,apellidoper,emailper,direccionper,telefonoper,usuarioper,contrasena,rol) VALUES(%s,%s,%s,%s,%s,%s,%s)",
+        cursor.execute("INSERT INTO personas(nombreper,apellidoper,emailper,direccionper,telefonoper,usuarioper,contrasena,rol) VALUES(%s,%s,%s,%s,%s,%s,%s,%s)",
                        (Nombres,Apellidos,Email,Direccion,Telefono,Usuario,Contrasenaencriptada,Rol))
         db.commit()
         flash('Usuario creado correctamente','success')
@@ -158,69 +159,95 @@ def eliminar_usuario(id):
     db.commit()
     return redirect(url_for('lista'))
 
-##-------------Registro Canciones--------##
+##-------------Lista de canciones----------##
 
-@app.route('/Registro', methods=['GET', 'POST'])
-def registro_cancion():
+@app.route("/LISTAR")
+def list_song(): 
+    cursor = db.cursor()
+    cursor.execute("SELECT titulo,artista,genero,precio,duracion,lanzamiento,img FROM canciones")
+    canciones = cursor.fetchall()
+
+    if canciones:
+        cancioneslist = []
+        for cancion in canciones:
+            #Convertir la imagen formato base64
+            imagen = base64.b64encode(cancion[6].decode('utf-8'))
+            #agregar los datos de la cancion a la lista
+            cancioneslist.append({
+                'titulo':cancion[0],
+                'artista':cancion[1],
+                'genero':cancion[2],
+                'precio':cancion[3],
+                'duracion':cancion[4],
+                'lanzamiento':cancion[5],
+                'imagen':imagen,
+            })
+    
+        return render_template("LISTAR.html", canciones = cancioneslist)
+    else:
+        return print("Canciones no encontradas")
+    
+
+##-------------Registro Canciones----------##
+
+@app.route("/REGISTRO", methods =['GET', 'POST'])
+def add_song():
+    cursor = db.cursor()
     if request.method == 'POST':
-       Titulo = request.form.get('titulo')
-       Artista = request.form.get('artista')
-       Genero = request.form.get('genero')
-       Precio = request.form.get('precio')
-       Duracion = request.form.get('duracion')
-       Lanzamiento = request.form.get('lanzamiento')
+        Titulo = request.form.get('titulo')  
+        Artista = request.form.get('artista')
+        Genero = request.form.get('genero')
+        Precio = request.form.get('precio')
+        Duracion = request.form.get('duracion')
+        Fecha = request.form.get('fecha')
+        Imagen = request.form.get('image')
 
-       cursor = db.cursor()
-       cursor.execute(
-           "SELECT * FROM canciones WHERE titulo = %s or artista = %s", (Titulo,Artista))
-       cursor.execute("INSERT INTO personas(titulo,artista,genero,precio,duracion,lanzamiento) VALUES (%s,%s,%s,%s,%s,%s)",
-        (Titulo,Artista,Genero,Precio,Duracion,Lanzamiento))
-       db.commit()
 
-       return redirect(url_for('registrar_cancion'))  # Redirigir a la página principal
-    return render_template("Registro.html")
+
+        cursor.execute("INSERT INTO canciones (titulo, artista, genero, precio, duracion, lanzamiento,img)VALUES(%s,%s,%s,%s,%s,%s,%s)",(Titulo,Artista,Genero,Precio,Duracion,Fecha,Imagen))
+        return redirect(url_for('add_song'))
+    db.commit()
+    return render_template("REGISTRO.html")
+
 
 ##----------Actualizar Canciones--------##
 
-@app.route('/actualizar/<int:id>',methods=['GET', 'POST'])
-def editar_cancion(id):
+@app.route("/ACTUALIZAR/<int:id>", methods=["POST", "GET"])
+def update_song(id):
     cursor = db.cursor()
-    if request.method == 'POST':
-        titulo = request.form.get('titulo')
-        artista = request.form.get('artista')
-        genero = request.form.get('genero')
-        precio = request.form.get('precio')
-        duracion = request.form.get('duracion')
-        lanzamiento = request.form.get('lanzamiento')
+    if request.method == "POST":
+        Titulou = request.form.get('Titu')  
+        Artistau = request.form.get('Arti')
+        Generou = request.form.get('Gene')
+        Preciou = request.form.get('Preci')
+        Duracionu = request.form.get('Dura')
+        Fechau = request.form.get('Lanz')
+        Imagenu = request.form.get('Imagen')
 
-        sql = "UPDATE canciones set titulo=%s,artista=%s,genero=%s,precio=%s,duracion=%s,lanzamiento=%s, where id_can=%s"
-        cursor.execute(sql,(titulo,artista,genero,precio,duracion,lanzamiento,id)) 
+        cursor.execute("update canciones set titulo=%s, artista=%s, genero=%s, precio=%s, duracion=%s, lanzamiento=%s,img=%s where id_can=%s",(Titulou,Artistau,Generou,Preciou,Duracionu,Fechau,Imagenu,id))
         db.commit()
-        
-        return redirect(url_for('listar'))
-    
-    else: 
+        return redirect(url_for('list_song'))
+    else:
         cursor = db.cursor()
-        cursor.execute('SELECT * FROM canciones WHERE id_canciones=%s' ,(id,))
+        cursor.execute("SELECT * FROM canciones WHERE id_can =%s", (id,))
         data = cursor.fetchall()
-
-        return render_template('actualizar.html', canciones=data[0])
+        return render_template("ACTUALIZAR.html", cancion=data[0])
     
 ##------------Eliminar Canciones-----------##
     
-@app.route("/eliminar/<int:id>", methods=['GET'])
-def eliminar_cancion(id):
-
+@app.route("/Eliminar_cancion/<int:id>", methods=["GET"])
+def delete_song(id):
     cursor = db.cursor()
-    cursor.execute('DELETE FROM canciones WHERE id_cancion = %s', (id,))
-    db.commit()
-    return redirect(url_for('listar'))
-
+    if request.method == "GET":
+        cursor.execute( "delete from canciones where id_can=%s" , (id,))
+        db.commit()
+        return redirect(url_for("list_song"))
+    
 ##------------------------------------##
 
 
 if __name__ == '__main__':
-    app.add_url_rule('/',view_func=lista)
+    app.add_url_rule('/',view_func=login)
     app.run(debug=True,port=5005)
 
 ##------------------------------------##
